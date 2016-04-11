997c6e6e2f18a1b7b964c0a3ad08a662bc327295
Jeff Joltes <jeff.joltes@gmail.com>
2016-04-11 12:18:01 -0600

    added zsh configuratiion baseline includind antigen setup

---
+++ zsh/config/exports.zsh
@@ -0,0 +1,16 @@
+# Setup terminal, and turn on colors
+export TERM=xterm-256color
+
+# Enable color in grep
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='3;33'
+
+# This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
+export ARCHFLAGS='-arch x86_64'
+
+export LESS='--ignore-case --raw-control-chars'
+export PAGER='less'
+export EDITOR='leafpad'
+
+# Virtual Environment Stuff
+export WORKON_HOME=$HOME/.virtualenvs
