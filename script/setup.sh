97ee18d82f3a67444c81087db7f148399ab905cc
Jeff Joltes <jeff.joltes@gmail.com>
2025-12-29 13:49:29 -0700

    pipes error message to std error

--- script/setup.sh
+++ script/setup.sh
@@ -17,7 +17,7 @@ echo ""
 info() { echo -e "\033[0;34m[INFO]\033[0m $1"; }
 success() { echo -e "\033[0;32m[OK]\033[0m $1"; }
 warn() { echo -e "\033[0;33m[WARN]\033[0m $1"; }
-error() { echo -e "\033[0;31m[ERROR]\033[0m $1"; }
+error() { echo -e "\033[0;31m[ERROR]\033[0m $1" >&2; }
 
 link_file() {
   local src="$1" dst="$2"
